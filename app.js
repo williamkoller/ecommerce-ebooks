a63f17d89f4061efa8b5c1150cce2ecea00a522c
William Koller <wkoller25@gmail.com>
2020-02-02 14:59:15 -0300

    tratamento com cors e body-parser

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const routeProducts = require('./routes/products');
 const routeRequests = require('./routes/requests');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false })); // apenas dados simples
+app.use(bodyParser.json()); // json de entrada no body
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // origem de permissão de controle de acesso para todos 
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+
+    next();
+});
+
 app.use('/products', routeProducts);
 app.use('/requests', routeRequests);
 
