bddaf87b7eaa43d209d4735781881145ce3d7005
William Koller <wkoller25@gmail.com>
2020-02-04 22:20:31 -0300

    manipulating api for uploading images

--- ecommerce Session.sql
+++
@@ -1,3 +0,0 @@
-select * from pedidos;
-
-select * from produtos;
--- routes/produtos.js
+++ routes/produtos.js
@@ -3,6 +3,34 @@ const express = require('express');
 // Iniciando o Express Router em uma constante chamada router
 const router = express.Router();
 const mysql = require('../mysql').pool;
+// Inicia o Multer
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './uploads/');
+    },
+    filename: function(req, file, callback) {
+        callback(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, callback) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+}
+
+// Salva todos os upload na pasta uploads
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 /**
  * Retorna todos os produtos
@@ -22,10 +50,11 @@ router.get('/', (req, res, next) => {
                             id_produto: prod.id_produto,
                             nome: prod.nome,
                             preco: prod.preco,
+                            imagem_produto: prod.imagem_produto,
                             request: {
                                 type: 'GET',
                                 description: 'Retorna os detalhes de um produto específico',
-                                url: 'http://localhost:3000/products/' + prod.id_produto
+                                url: 'http://localhost:3000/produtos/' + prod.id_produto
                             }
                         }
                     })
@@ -39,11 +68,17 @@ router.get('/', (req, res, next) => {
 /**
  * Insere um produto
  */
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('produto_imagem'), (req, res, next) => {
+    console.log(req.file);
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
-            'INSERT INTO produtos (nome, preco) VALUES (?,?)', [req.body.nome, req.body.preco],
+            'INSERT INTO produtos (nome, preco, imagem_produto) VALUES (?,?, ?)', [
+                req.body.nome,
+                req.body.preco,
+                req.file.path
+            ],
+
             // callback
             (error, result, field) => {
                 conn.release(); // libera a conexão
@@ -54,10 +89,11 @@ router.post('/', (req, res, next) => {
                         id_produto: result.id_produto,
                         nome: req.body.nome,
                         preco: req.body.preco,
+                        imagem_produto: req.file.path,
                         request: {
                             type: 'GET',
                             description: 'Retorna todos os produtos',
-                            url: 'http://localhost:3000/products'
+                            url: 'http://localhost:3000/produtos'
                         }
                     }
                 }
@@ -90,10 +126,11 @@ router.get('/:id_produto', (req, res, next) => {
                         id_produto: result[0].id_produto,
                         nome: result[0].nome,
                         preco: result[0].preco,
+                        imagem_produto: result[0].imagem_produto,
                         request: {
                             type: 'GET',
                             description: 'Retorna um produto',
-                            url: 'http://localhost:3000/products'
+                            url: 'http://localhost:3000/produtos'
                         }
                     }
                 }
@@ -131,7 +168,7 @@ router.patch('/', (req, res, next) => {
                         request: {
                             type: 'GET',
                             description: 'Retorna os detalhes de um produto específico',
-                            url: 'http://localhost:3000/products/' + req.body.id_produto
+                            url: 'http://localhost:3000/produtos/' + req.body.id_produto
                         }
                     }
                 }
@@ -158,7 +195,7 @@ router.delete('/', (req, res, next) => {
                     request: {
                         type: 'POST',
                         description: 'Insere um produto',
-                        url: 'http://localhost:3000/products',
+                        url: 'http://localhost:3000/produtos',
                         body: {
                             nome: 'String',
                             preco: 'Number'
