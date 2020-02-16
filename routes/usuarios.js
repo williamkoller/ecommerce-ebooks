b202198bbec155f776741653e41d6be0b34834f4
William Koller <wkoller25@gmail.com>
2020-02-16 15:53:31 -0300

    adding jwt

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((err, conn) => {
@@ -33,5 +34,43 @@ router.post('/cadastro', (req, res, next) => {
         })
     });
 });
+
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        const query = `SELECT * FROM usuarios WHERE email = ?`;
+        conn.query(query, [req.body.email], (error, results, fields) => {
+            conn.release();
+            if (error) { return res.status(500).send({ error: error }) }
+            if (results.length < 1) {
+                return res.status(401).send({
+                    message: 'Falha na autenticação'
+                })
+            }
+            bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
+                if (err) {
+                    return res.status(401).send({
+                        message: 'Falha na autenticação'
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        id_usuario: results[0].id_usuario,
+                        email: results[0].email
+                    }, process.env.JWT_KEY, {
+                        expiresIn: "1h"
+                    });
+                    return res.status(200).send({
+                        message: 'Autenticado com sucesso!',
+                        token: token
+                    });
+                }
+                return res.status(401).send({
+                    message: 'Falha na autenticação'
+                });
+            });
+        });
+    });
+})
 
 module.exports = router;
