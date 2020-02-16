89d6f735f01613b684e80a3e55a7a1e2cb9a0b4f
William Koller <wkoller25@gmail.com>
2020-02-16 15:55:08 -0300

    adding middleware login

---
+++ middleware/login.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+exports.obrigatorio = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decode = jwt.verify(token, process.env.JWT_KEY)
+        req.usuario = decode;
+        next();
+    } catch (error) {
+        return res.status(401).send({ message: 'Falha na autenticação' })
+    }
+}
+
+exports.opcional = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decode = jwt.verify(token, process.env.JWT_KEY)
+        req.usuario = decode;
+        next();
+    } catch (error) {
+        next(); // mesmo sem o token ele continua
+    }
+}
