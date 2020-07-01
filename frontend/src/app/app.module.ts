a0615a8680170a02c979b72116ef9c9182917f68
William Koller <wkoller25@gmail.com>
2020-07-01 20:17:01 -0300

    feat: add compoments on module of application

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopBarComponent } from './header/top-bar/top-bar.component';
+import { HeaderMainComponent } from './header/header-main/header-main.component';
+import { MainNavComponent } from './header/main-nav/main-nav.component';
+import { PageMenuComponent } from './header/page-menu/page-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopBarComponent,
+    HeaderMainComponent,
+    MainNavComponent,
+    PageMenuComponent
   ],
   imports: [
     BrowserModule
